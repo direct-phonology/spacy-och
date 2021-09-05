9a87f21838c898a155f4bed3272aafc9e3e1bda3
Nick Budak <nbudak@princeton.edu>
2021-09-05 15:15:33 -0400

    Add basic tests and ci workflow

---
+++ tests/test_lang.py
@@ -0,0 +1,18 @@
+"""Test the Old Chinese language object using spaCy."""
+
+from unittest import TestCase
+
+import spacy
+
+
+class TestLanguage(TestCase):
+    def test_import(self) -> None:
+        """language object should be able to be imported"""
+        nlp = spacy.blank("och")
+        self.assertEqual(nlp.lang, "och")
+
+    def test_tokenize(self) -> None:
+        """should tokenize by character without a model defined"""
+        nlp = spacy.blank("och")
+        tokens = [str(token) for token in nlp("北冥有魚，其名為鯤。")]
+        self.assertEqual(tokens, ["北", "冥", "有", "魚", "，", "其", "名", "為", "鯤", "。"])
