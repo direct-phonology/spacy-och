0d4f8062048c8385c58245ec2a758c0fd91bd97b
Nick Budak <nbudak@princeton.edu>
2021-09-05 14:42:28 -0400

    Basic language config

---
+++ och/__init__.py
@@ -0,0 +1,50 @@
+from typing import Callable
+
+import spacy
+from spacy.lang.zh import ChineseTokenizer, Segmenter
+from spacy.language import Language
+from spacy.util import load_config_from_str, registry
+
+from .lex_attrs import LEX_ATTRS
+from .stop_words import STOP_WORDS
+
+__version__ = "0.1.0"
+
+DEFAULT_CONFIG = """
+    [nlp]
+
+    [nlp.tokenizer]
+    @tokenizers = "spacy.zh.OldChineseTokenizer"
+"""
+
+# For now, just extend the default Chinese tokenizer
+class OldChineseTokenizer(ChineseTokenizer):
+    pass
+
+
+# Without a model, just segment by character
+# https://spacy.io/api/tokenizer
+@registry.tokenizers("spacy.och.OldChineseTokenizer")
+def create_old_chinese_tokenizer() -> Callable[[Language], OldChineseTokenizer]:
+    def old_chinese_tokenizer_factory(nlp: Language) -> OldChineseTokenizer:
+        return OldChineseTokenizer(nlp, segmenter=Segmenter.char)
+
+    return old_chinese_tokenizer_factory
+
+
+# https://spacy.io/api/language#defaults
+class OldChineseDefaults(Language.Defaults):
+    config = load_config_from_str(DEFAULT_CONFIG)
+    lex_attr_getters = LEX_ATTRS
+    stop_words = STOP_WORDS
+    writing_system = {"direction": "ltr", "has_case": False, "has_letters": False}
+
+
+# https://spacy.io/api/language#class-attributes
+@spacy.registry.languages("och")
+class OldChinese(Language):
+    lang = "och"
+    Defaults = OldChineseDefaults
+
+
+__all__ = ["OldChinese"]
---
+++ och/examples.py
@@ -0,0 +1,12 @@
+"""
+Example sentences to test spaCy and its language models.
+>>> from spacy.lang.och.examples import sentences
+>>> docs = nlp.pipe(sentences)
+"""
+
+
+sentences = [
+    "子曰：「上下无常，非為邪也。進退无恆，非離群也。君子進德脩業、欲及時也，故无咎。」",
+    "北冥有魚，其名為鯤。鯤之大，不知其幾千里也。化而為鳥，其名為鵬。",
+    "仲子生而有文在其手，曰為魯夫人，故仲子歸于我。",
+]
