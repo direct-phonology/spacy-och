3746a19daef7a241d6d8af8e02d0b4ddc4261413
Nick Budak <budak@stanford.edu>
2022-02-22 22:02:33 -0800

    Adjust lookup tests

--- tests/test_lookups.py
+++ tests/test_lookups.py
@@ -8,13 +8,13 @@ import srsly
 
 class TestLookups(TestCase):
     def test_pinyin_lookup(self) -> None:
-        """character pinyin lookup data should be available"""
-        py_lookup_path = spacy.registry.lookups.get("och")()["pinyin_lookup"]
+        """character pinyin data should be available"""
+        py_lookup_path = spacy.registry.lookups.get("och")()["pinyin"]
         pinyin = srsly.read_json(py_lookup_path)
         self.assertEqual(pinyin["㐁"], ["tiàn"])
 
-    def test_variants_lookup(self) -> None:
-        """character variant lookup data should be available"""
-        var_lookup_path = spacy.registry.lookups.get("och")()["variants_lookup"]
-        variants = srsly.read_json(var_lookup_path)
-        self.assertEqual(variants["䡵"]["simplified"], ["𫟦"])
+    def test_numbers_lookup(self) -> None:
+        """character numeric data should be available"""
+        var_lookup_path = spacy.registry.lookups.get("och")()["numbers"]
+        numeric = srsly.read_json(var_lookup_path)
+        self.assertEqual(numeric["七"], {"value": 7, "type": "primary"})
